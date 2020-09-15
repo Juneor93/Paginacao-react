c4af72272d9b3cddea0119740e38848d31c1b548
Paulo <pauloh16.pdr@gmail.com>
2020-09-15 00:54:21 -0300

    Adicionar atualizações para cards

--- src/Componentes/Card.jsx
+++ src/Componentes/Card.jsx
@@ -10,8 +10,25 @@ export default (props) => (
         <div className="infos-card col-8">
             <h2>{props.titulo}</h2>
             <span className="autor">{props.autor}</span>
-            <p>{props.resumo}</p>
+            <p>{limitarResumo(props.resumo, 300)}</p>
             <a href="#">Ver mais</a>
         </div>
     </div>
 );
+
+function limitarResumo(resumo, limite) {
+    const palavras = resumo.split(" ");
+
+    let soma = palavras.length;
+    const novoResumo = [];
+
+    for(let i = 0; i < palavras.length; i++) {
+        soma += palavras[i].length;
+
+        if (soma <= limite) {
+            novoResumo.push(palavras[i]);
+        }
+    }
+
+    return `${novoResumo.join(" ")}...`;
+}
